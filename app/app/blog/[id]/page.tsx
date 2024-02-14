7d8d132321293b9bb9f36cd16d07534bfec6f435
hypulse <sjinsilval28@naver.com>
2024-02-14 16:26:57 +0900

    Blog page SSR and metadata generation

--- app/app/blog/[id]/page.tsx
+++ app/app/blog/[id]/page.tsx
@@ -1,3 +1,29 @@
-export default function Page() {
-  return null;
+import { PageProps } from "@/types/types";
+import getPost from "@/utils/getData/getPost";
+import { getMetaTitle } from "@/utils/metadata-utils";
+import { Metadata } from "next";
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const id = params.id;
+
+  const data = await getPost(id);
+
+  return {
+    title: getMetaTitle(data.title),
+  };
+}
+
+export default async function Page({ params: { id } }: PageProps) {
+  const data = await getPost(id);
+
+  return (
+    <article
+      className="prose"
+      dangerouslySetInnerHTML={{
+        __html: `<h1>${data.title}</h1>${data.content}`,
+      }}
+    />
+  );
 }
