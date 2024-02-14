153f8faf3783c3d0b58ff7f8275ccd6194de5bef
hypulse <sjinsilval28@naver.com>
2024-02-14 16:55:47 +0900

    Add Footer and Navbar components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+      <aside>
+        <p></p>
+      </aside>
+    </footer>
+  );
+}
