91a88b6dee58453846254201f657813a7240b0ba
hypulse <sjinsilval28@naver.com>
2024-02-14 16:26:17 +0900

    Add GET route to fetch a post by ID

---
+++ app/app/api/posts/[id]/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import PocketBase from "pocketbase";
+
+const pb = new PocketBase(process.env.POCKETBASE_URL);
+
+export async function GET(
+  req: Request,
+  {
+    params: { id },
+  }: {
+    params: {
+      id: string;
+    };
+  }
+) {
+  try {
+    const post = await pb.collection("posts").getOne(id, { expand: "tags" });
+    if (post.type !== "article" || post.status !== "published") {
+      throw new Error("Post not found");
+    }
+    return NextResponse.json(post);
+  } catch (err) {
+    console.error("Failed to fetch post:", err);
+    return new Response(JSON.stringify({ error: "Failed to fetch post" }), {
+      status: 500,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+}
