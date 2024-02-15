1ddb457bc35cd8cedf2fa93a519b07cca2f67ab2
hypulse <sjinsilval28@naver.com>
2024-02-15 14:38:48 +0900

    Refactor layout component

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { getMetaTitle } from "@/utils/metadata-utils";
-import Footer from "@/components/Footer";
-import Navbar from "@/components/Navbar";
+import Layout from "@/components/Layout";
+import { BLOG_NAME } from "@/types/constants";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: getMetaTitle(),
+  title: BLOG_NAME,
 };
 
 export default function RootLayout({
@@ -19,9 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div className="max-w-screen-sm mx-auto px-4 py-16">{children}</div>
-        <Footer />
+        <Layout>{children}</Layout>
       </body>
     </html>
   );
---
+++ app/components/Layout/index.tsx
@@ -0,0 +1,12 @@
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Navbar />
+      <main className="max-w-screen-sm mx-auto px-4 py-16">{children}</main>
+      <Footer />
+    </>
+  );
+}
