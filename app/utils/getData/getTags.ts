56dba682332861eedc0dd6de08d53befe4d839b4
hypulse <sjinsilval28@naver.com>
2024-02-16 10:51:32 +0900

    Handle 429 error in tag route and getData function

--- app/utils/getData/getTags.ts
+++ app/utils/getData/getTags.ts
@@ -3,6 +3,12 @@ import { Tag } from "@/types/types-tag";
 async function getTags(): Promise<Tag[]> {
   const res = await fetch((process.env.APP_URL || "") + "/api/tags");
 
+  if (res.status === 429) {
+    return new Promise((resolve) => {
+      resolve([]);
+    });
+  }
+
   if (!res.ok) {
     throw new Error("Failed to fetch data");
   }
