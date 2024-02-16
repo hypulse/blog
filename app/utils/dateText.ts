29af681fbe5925479abd26f1a5a07e1490add10d
hypulse <sjinsilval28@naver.com>
2024-02-16 13:38:11 +0900

    Remove weekday option from dateText function

--- app/utils/dateText.ts
+++ app/utils/dateText.ts
@@ -1,7 +1,6 @@
 const dateText = (created: string) => {
   const event = new Date(created);
   const options: Intl.DateTimeFormatOptions = {
-    weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
