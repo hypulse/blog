fc8eba2bad2bf17c26eabe2f4becf99eeeb9a672
hypulse <sjinsilval28@naver.com>
2024-02-16 10:31:44 +0900

    Sort tags alphabetically

--- app/components/TagList.tsx
+++ app/components/TagList.tsx
@@ -1,9 +1,13 @@
 import { Tag } from "@/types/types-tag";
 
 export default function TagList({ tags }: { tags?: Tag[] }) {
+  if (!tags || tags.length === 0) return null;
+
+  tags = tags.sort((a, b) => a.name.localeCompare(b.name));
+
   return (
     <div className="flex flex-wrap gap-2">
-      {tags?.map(({ id, name }) => (
+      {tags.map(({ id, name }) => (
         <span key={id} className="badge">
           {name}
         </span>
