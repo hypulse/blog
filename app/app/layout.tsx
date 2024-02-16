dd19d6b6736b68f8fd42919082a2a785c20d9b9a
hypulse <sjinsilval28@naver.com>
2024-02-16 18:03:24 +0900

    Add metadataBase URL to layout.tsx

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -7,6 +7,7 @@ import { BLOG_NAME } from "@/utils/constants";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("http://localhost:3000"),
   title: BLOG_NAME,
 };
 
