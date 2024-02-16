2583dc37aa44d07c4f672e5cf910d2d49d3c7616
hypulse <sjinsilval28@naver.com>
2024-02-16 14:57:52 +0900

    Add margin-top to Footer component

--- app/components/Layout/Footer.tsx
+++ app/components/Layout/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded">
+    <footer className="footer footer-center p-10 bg-base-200 text-base-content rounded mt-16">
       <nav>
         <div className="grid grid-flow-col gap-4">
           <Link href="https://github.com/hypulse">
