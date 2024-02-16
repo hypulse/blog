65fa39bb433d86285ec51b5d2d55d5b3e2acf40a
hypulse <sjinsilval28@naver.com>
2024-02-16 17:43:53 +0900

    Refactor search page and search box components

--- app/components/Search/SearchBox.tsx
+++ app/components/Search/SearchBox.tsx
@@ -1,44 +1,104 @@
 "use client";
 
+import { Tag } from "@/types/types-tag";
+import getTags from "@/utils/getData/getTags";
+import tagsSort from "@/utils/tagsSort";
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { useEffect, useState } from "react";
 
 export default function SearchBox() {
-  const ref = useRef<HTMLInputElement>(null);
   const router = useRouter();
+  const [query, setQuery] = useState<string>("");
+  const [tags, setTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+  useEffect(() => {
+    getTags().then(setTags);
+  }, []);
+
+  const addTag = (tag: Tag) => {
+    if (selectedTags.some((t) => t.id === tag.id)) {
+      return;
+    }
+    setSelectedTags((prev) => [...prev, tag]);
+  };
+
+  const removeTag = (tag: Tag) => {
+    setSelectedTags((prev) => prev.filter((t) => t.id !== tag.id));
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    const query = ref.current?.value;
-    if (query) {
-      router.push(`/blog?q=${query}`);
-    }
+    router.push(
+      `/blog?q=${query}&tags=${selectedTags.map((t) => t.id).join(",")}`
+    );
   };
 
   return (
-    <form onSubmit={handleSubmit} className="relative">
-      <label htmlFor="search-input" className="sr-only">
-        Search
-      </label>
-      <input
-        type="search"
-        id="search-input"
-        placeholder="Search"
-        className="input input-bordered w-full pr-10"
-        ref={ref}
-      />
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="1em"
-        height="1em"
-        viewBox="0 0 24 24"
-        className="opacity-70 absolute top-3 right-3"
-      >
-        <path
-          fill="currentColor"
-          d="M9.5 3A6.5 6.5 0 0 1 16 9.5c0 1.61-.59 3.09-1.56 4.23l.27.27h.79l5 5l-1.5 1.5l-5-5v-.79l-.27-.27A6.516 6.516 0 0 1 9.5 16A6.5 6.5 0 0 1 3 9.5A6.5 6.5 0 0 1 9.5 3m0 2C7 5 5 7 5 9.5S7 14 9.5 14S14 12 14 9.5S12 5 9.5 5"
-        ></path>
-      </svg>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="mb-5">
+        <div className="input input-bordered h-auto flex flex-col mb-3">
+          <div className="flex flex-wrap gap-2">
+            {tagsSort(selectedTags).map((tag) => (
+              <button
+                key={tag.id}
+                className="btn btn-sm"
+                onClick={() => removeTag(tag)}
+              >
+                {tag.name}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                  className="opacity-70"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12z"
+                  ></path>
+                </svg>
+              </button>
+            ))}
+          </div>
+          <input
+            type="search"
+            placeholder="Search"
+            className="h-12"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          ></input>
+        </div>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={!(query.length > 0 || selectedTags.length > 0)}
+          >
+            Search
+          </button>
+        </div>
+      </form>
+      <div className="flex flex-wrap gap-x-4 gap-y-2">
+        {tagsSort(tags)
+          .filter((tag) => !selectedTags.some((t) => t.id === tag.id))
+          .map((tag) => (
+            <button key={tag.id} className="btn" onClick={() => addTag(tag)}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1em"
+                height="1em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M5.5 7A1.5 1.5 0 0 1 4 5.5A1.5 1.5 0 0 1 5.5 4A1.5 1.5 0 0 1 7 5.5A1.5 1.5 0 0 1 5.5 7m15.91 4.58l-9-9C12.05 2.22 11.55 2 11 2H4c-1.11 0-2 .89-2 2v7c0 .55.22 1.05.59 1.41l8.99 9c.37.36.87.59 1.42.59c.55 0 1.05-.23 1.41-.59l7-7c.37-.36.59-.86.59-1.41c0-.56-.23-1.06-.59-1.42"
+                ></path>
+              </svg>
+              {tag.name}
+            </button>
+          ))}
+      </div>
+    </>
   );
 }
