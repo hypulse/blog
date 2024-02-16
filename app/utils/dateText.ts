8847760bb3e60c0eb4fd0c7db3d1fd032e8a4332
hypulse <sjinsilval28@naver.com>
2024-02-16 10:33:25 +0900

    Refactor dateText function

--- app/utils/dateText.ts
+++ app/utils/dateText.ts
@@ -1,11 +1,13 @@
 const dateText = (created: string) => {
-  const date = new Date(created);
-
-  return date.toLocaleDateString("en-US", {
+  const event = new Date(created);
+  const options: Intl.DateTimeFormatOptions = {
+    weekday: "long",
     year: "numeric",
     month: "long",
     day: "numeric",
-  });
+  };
+
+  return event.toLocaleDateString(undefined, options);
 };
 
 export default dateText;
