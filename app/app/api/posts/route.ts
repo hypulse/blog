87347020fa5a479b0f27553a3405f74a0df6b610
hypulse <sjinsilval28@naver.com>
2024-02-18 21:32:42 +0900

    Refactor codes to sanitize input and apply filters

--- app/app/api/posts/route.ts
+++ app/app/api/posts/route.ts
@@ -9,40 +9,38 @@ export async function GET(req: Request) {
     const page = new URL(req.url).searchParams.get("page") || "1";
     const type = new URL(req.url).searchParams.get("type") || "article";
     let q = new URL(req.url).searchParams.get("q") || "";
-    q = escapeHtml(q);
     let tags = new URL(req.url).searchParams.get("tags") || "";
-    tags = escapeHtml(tags);
-
-    console.log(q);
 
     // Sanitize input
-    const filters = [];
-    // Post state for user is always published
+    q = escapeHtml(q);
+    tags = escapeHtml(tags);
     const stateFilter = pb.filter("state = {:state}", {
       state: "published",
     });
-    filters.push(stateFilter);
-    // Post type(article, snippet, etc.)
-    // Currently, only state "article" is supported
     const typeFilter = pb.filter("type = {:type}", { type });
-    filters.push(typeFilter);
-    // Search query
+    let queryFilter: string[] = [];
+    let tagsFilter: string[] = [];
     if (q) {
-      const queryFilter = pb.filter("(title ~ {:q} || content ~ {:q})", {
-        q,
-      });
-      filters.push(queryFilter);
+      queryFilter = [
+        pb.filter("(title ~ {:q} || content ~ {:q})", {
+          q,
+        }),
+      ];
     }
-    // Search by tags
     if (tags) {
-      const tagsFilter = tags
+      tagsFilter = tags
         .split(",")
         .map((tag) => pb.filter("tags ~ {:tag}", { tag }));
-      filters.push(...tagsFilter);
     }
+    const filter = [
+      stateFilter,
+      typeFilter,
+      ...queryFilter,
+      ...tagsFilter,
+    ].join(" && ");
 
     const post = await pb.collection("posts").getList(Number(page), 10, {
-      filter: filters.join(" && "),
+      filter,
       fields:
         "id,created,updated,title,thumbnail,type,state,tags,expand,content:excerpt(200,true)",
       sort: "-updated",
