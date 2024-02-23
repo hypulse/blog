cc7e48e68ca63264d9934ec31c05da90c5e5246b
hypulse <sjinsilval28@naver.com>
2024-02-23 11:25:15 +0900

    Add RichTextEditor component

--- app/components/Admin/RichTextEditor/RichTextEditor.tsx
+++ app/components/Admin/RichTextEditor/RichTextEditor.tsx
@@ -1,3 +1,47 @@
+"use client";
+
+import MdEditor from "react-markdown-editor-lite";
+import "react-markdown-editor-lite/lib/index.css";
+import { Marked } from "marked";
+import { editImage, onImageUpload } from "@/utils/editor-utils";
+import { useEffect, useRef } from "react";
+import { POST_STATES } from "@/utils/constants";
+
 export default function RichTextEditor() {
-  return <div>RichTextEditor</div>;
+  const editor = useRef<MdEditor>(null);
+
+  useEffect(() => {
+    window.onbeforeunload = () => {
+      return "Are you sure you want to leave?";
+    };
+    return () => {
+      window.onbeforeunload = null;
+    };
+  }, []);
+
+  return (
+    <>
+      <MdEditor
+        ref={editor}
+        style={{ height: "500px" }}
+        renderHTML={(text) => new Marked().parse(text)}
+        onImageUpload={(file: File) =>
+          onImageUpload(file).then((dataUrl) => editImage(dataUrl, 800, 600))
+        }
+      />
+      <div className="flex justify-end">
+        <div className="join">
+          <select className="select select-bordered join-item">
+            <option disabled selected>
+              Select a state
+            </option>
+            {POST_STATES.map((state) => (
+              <option key={state}>{state}</option>
+            ))}
+          </select>
+          <button className="btn btn-primary join-item">Save</button>
+        </div>
+      </div>
+    </>
+  );
 }
